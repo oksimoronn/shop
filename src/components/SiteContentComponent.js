acd173f63808a6353c515674a2dc688d54c84901
oksimoronn <zvonetola@gmail.com>
2023-04-23 15:09:31 +0200

    dinamički stvoren nav

--- src/components/SiteContentComponent.js
+++ src/components/SiteContentComponent.js
@@ -2,13 +2,64 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import GamingNavComponent from "./navComponents/GamingNavComponent";
 import InformatikaNavComponent from "./navComponents/InformatikaNavComponent";
+//import { key } from "localforage";
 
 export default function SiteContentComponent() {
   const initialvalues = [
     { el: "gaingm", state: false },
     { el: "infom", state: false },
     { el: "mob", state: false },
+    { el: "tv", state: false },
+    { el: "veAparati", state: false },
+    { el: "maAparati", state: false },
+    { el: "njega", state: false },
+    { el: "dronovi", state: false },
+    { el: "dom", state: false },
+    { el: "outlet", state: false },
+    { el: "sport", state: false },
+    { el: "psBonovi", state: false },
   ];
+
+  const titleValueas = [
+    { el: "gaingm", name: "Gaming", adress: "/gaming", state: false },
+    { el: "infom", name: "Informatika", adress: "/informatika", state: false },
+    {
+      el: "mob",
+      name: "Mobiteli, tablet i satovi",
+      adress: "/mob",
+      state: false,
+    },
+    { el: "tv", name: "Televizori i audio", adress: "/tv", state: false },
+    {
+      el: "veAparati",
+      name: "Veliki kućanski aparati",
+      adress: "/velikiKucanskiAparati",
+      state: false,
+    },
+    {
+      el: "maAparati",
+      name: "Mali kućanski aparati",
+      adress: "/maliKucanskiAparati",
+      state: false,
+    },
+    { el: "njega", name: "Osobna njega", adress: "/njega", state: false },
+    {
+      el: "dronovi",
+      name: "Dronovi, kamere i navigacija",
+      adress: "/dronoviKamereNavigacija",
+      state: false,
+    },
+    {
+      el: "dom",
+      name: " Dom, vrt i alati",
+      adress: "/domVrtAlat",
+      state: false,
+    },
+    { el: "outlet", name: "Outlet", adress: "/outlet", state: false },
+    { el: "sport", name: "Sport", adress: "/sport", state: false },
+    { el: "psBonovi", name: "PS bonovi", adress: "/psBonovi", state: false },
+  ];
+
   const [gaming, setGaming] = useState(initialvalues);
 
   const mouseHover = (el) => {
@@ -32,95 +83,48 @@ export default function SiteContentComponent() {
         }
       })
     );
-    //setGaming(false);
   };
 
   return (
     <div className="contentComponent">
       <ul>
-        {/* MORAŠ DODATI RUTEE ZA OVO SVE I POPRAVITI  to=""*/}
-        <li
-          onMouseOver={() => {
-            mouseHover("gaingm");
-          }}
-        >
-          <NavLink className="navLink" to="/about">
-            Gaming
-          </NavLink>
-        </li>
-        <li
-          onMouseOver={() => {
-            mouseHover("infom");
-          }}
-        >
-          <NavLink className="navLink" to="/about">
-            Informatika
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Mobiteli, tablet i satovi
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Televizori i audio
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Veliki kućanski aparati
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Mali kućanski aparati
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Osobna njega
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Dronovi, kamere i navigacija
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Dom, vrt i alati
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Outlet
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Sport
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className="navLink" to="/about">
-            Ps bonovi
-          </NavLink>
-        </li>
+        {titleValueas.map((el) => (
+          <li
+            key={el.adress}
+            onMouseOver={() => {
+              mouseHover(el.el);
+            }}
+          >
+            <NavLink className="navLink" to={el.adress}>
+              {el.name}
+            </NavLink>
+          </li>
+        ))}
       </ul>
       <hr></hr>
-      {gaming[0].state && (
+
+      {gaming.map(
+        (el) =>
+          el.state && (
+            <GamingNavComponent
+              key={el.el}
+              mouseHover={() => mouseHover(el.el)}
+              mouseOut={() => mouseOut(el.el)}
+            />
+          )
+      )}
+      {/*gaming[0].state && (
         <GamingNavComponent
           mouseHover={() => mouseHover("gaingm")}
           mouseOut={() => mouseOut("gaingm")}
         />
-      )}
-      {gaming[1].state && (
+      )*/}
+      {/*gaming[1].state && (
         <InformatikaNavComponent
           mouseHover={() => mouseHover("infom")}
           mouseOut={() => mouseOut("infom")}
         />
-      )}
+      )*/}
     </div>
   );
 }
--- src/components/navComponents/GamingNavComponent.js
+++ src/components/navComponents/GamingNavComponent.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-//onMouseOut={mouseOut}
+
 export default function GamingNavComponent({ mouseHover, mouseOut }) {
   return (
     <>
-      <hr></hr>
       <div
         className="gamingContent"
         onMouseOver={mouseHover}
